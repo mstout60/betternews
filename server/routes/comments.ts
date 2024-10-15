2da52d652646f0bd8bb6fd4fb816f56c59dfa8df
mstout60 <mstout@eprospector.com>
2024-10-15 13:13:08 -0500

    Implement creating nested comments and getting nested comments

--- server/routes/comments.ts
+++ server/routes/comments.ts
@@ -2,11 +2,18 @@ import { db } from "@/adapter";
 import type { Context } from "@/context";
 import { commentsTable } from "@/db/schemas/comments";
 import { postsTable } from "@/db/schemas/post";
+import { commentUpvotesTable } from "@/db/schemas/upvotes";
 import { getISOFormatDateQuery } from "@/lib/utils";
 import { loggedIn } from "@/middleware/loggedIn";
-import { createCommentSchema, type Comment, type SuccessResponse } from "@/shared/types";
+import {
+    createCommentSchema,
+    paginationSchema,
+    type Comment,
+    type PaginatedResponse,
+    type SuccessResponse
+} from "@/shared/types";
 import { zValidator } from "@hono/zod-validator";
-import { eq, sql } from "drizzle-orm";
+import { and, asc, countDistinct, desc, eq, sql } from "drizzle-orm";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { z } from "zod";
@@ -77,11 +84,120 @@ export const commentsRouter = new Hono<Context>()
             data: {
                 ...comment,
                 childComments: [],
-                commentUpvotes:[],
+                commentUpvotes: [],
                 author: {
                     username: user.username,
                     id: user.id,
                 }
             } as Comment,
         })
+    })
+    .post("/:id/upvote", loggedIn, zValidator("param", z.object({ id: z.coerce.number() })), async (c) => {
+        const { id } = c.req.valid("param");
+        const user = c.get("user")!;
+
+        let pointsChange: -1 | 1 = 1;
+
+        const points = await db.transaction(async (tx) => {
+            const [existingUpvote] = await tx
+                .select()
+                .from(commentUpvotesTable)
+                .where(
+                    and(
+                        eq(commentUpvotesTable.commentId, id),
+                        eq(commentUpvotesTable.userId, user.id),
+                    ),
+                )
+                .limit(1);
+
+            pointsChange = existingUpvote ? -1 : 1;
+
+            const [updated] = await tx.update(commentsTable)
+                .set({
+                    points: sql`${commentsTable.points} + ${pointsChange}`
+                })
+                .where(
+                    eq(commentsTable.id, id),
+                )
+                .returning({ points: commentsTable.points });
+
+            if (!updated) {
+                throw new HTTPException(404, { message: "Comment not found" });
+            }
+
+            if (existingUpvote) {
+                await tx
+                    .delete(commentUpvotesTable)
+                    .where(
+                        eq(commentUpvotesTable.id, existingUpvote.id),
+                    );
+            } else {
+                await tx
+                    .insert(commentUpvotesTable)
+                    .values({ commentId: id, userId: user.id });
+            }
+
+            return updated.points;
+        });
+
+        return c.json<SuccessResponse<{ count: number, commentUpvotes: { userId: string }[] }>>({
+            success: true,
+            message: "Comment updated",
+            data: {
+                count: points,
+                commentUpvotes: pointsChange === 1 ? [{ userId: user.id }] : []
+            },
+        }, 200);
+    })
+    .get("/:id/comments", zValidator("param", z.object({ id: z.coerce.number() })), zValidator("query", paginationSchema), async (c) => {
+        const user = c.get("user");
+        const { id } = c.req.valid("param");
+        const { limit, page, sortBy, order } = c.req.valid("query");
+        const offset = (page - 1) * limit;
+
+        const sortByColumn = sortBy === "points" ? commentsTable.points : commentsTable.createdAt;
+        const sortOrder = order === "desc" ? desc(sortByColumn) : asc(sortByColumn);
+
+        const [count] = await db
+            .select({ count: countDistinct(commentsTable.id) })
+            .from(commentsTable)
+            .where(eq(commentsTable.parentCommentId, id));
+
+        const comments = await db.query.comments.findMany({
+            where: (
+                and(
+                    eq(commentsTable.parentCommentId, id),
+                )
+            ),
+            orderBy: sortOrder,
+            limit: limit,
+            offset: offset,
+            with: {
+                author: {
+                    columns: {
+                        username: true,
+                        id: true,
+                    },
+                },
+                commentUpvotes: {
+                    columns: {
+                        userId: true,
+                    },
+                    where: eq(commentUpvotesTable.userId, user?.id ?? ""),
+                    limit: 1,
+                },
+            },
+            extras: {
+                createdAt: getISOFormatDateQuery(commentsTable.createdAt).as("created_at"),
+            },
+        });
+        return c.json<PaginatedResponse<Comment[]>>({
+            success: true,
+            message: "Comments fetched",
+            data: comments as Comment[],
+            pagination: {
+                page,
+                totalPages: Math.ceil(count.count / limit) as number,
+            },
+        }, 200);
     });
